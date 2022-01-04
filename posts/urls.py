7daaeb55318a7d7a3fcd3086fb461efcf8ec265d
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-01-04 16:06:44 -0300

    Lionks, categories and search coded

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostIndex.as_view(), name='index'),
-    path('categories/<str>:category', views.PostCategory.as_view(), name='categories'),
+    path('categories/<category>:category', views.PostCategory.as_view(), name='categories'),
     path('search/', views.PostSearch.as_view(), name='search'),
     path('post/<int:id>', views.PostDetails.as_view(), name='post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,19 +2,42 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
-
+from django.db.models import Q, Count, Case, When
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 3
+    paginate_by = 6
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(published=True)
+        qs = qs.annotate(numcomments=Count(Case(When(comment__published=True, then=1))))
+        return qs
+
 class PostSearch(PostIndex):
-    pass
+    template_name = 'posts/search.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        searched = self.request.GET.get('text')
+        if not searched:
+            return qs
+        qs = qs.filter(Q(title__icontains=searched) | Q(content__icontains=searched))
+        # qs = qs.annotate(numcomments=Count(Case(When(comment__published=True, then=1))))
+        return qs
 
 class PostCategory(PostIndex):
-    pass
+    template_name = 'posts/categories.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.kwargs.get('category', None)
+        if not category:
+            return qs
+        qs = qs.filter(catpost__catname__iexact=category)
+        return qs
 
 class PostDetails(UpdateView):
     pass
