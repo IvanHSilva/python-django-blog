68888a69ece56d66f616443897a71a8b658bba03
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-01-18 14:09:39 -0300

    Image post auto-resize

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from categories.models import Category
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
+from PIL import Image
 
 class Post(models.Model):
     title = models.CharField(max_length=150, verbose_name='Título')
@@ -15,3 +17,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.imgsresize(self.image.name, 800)
+
+    @staticmethod
+    def imgsresize(img, newwidth):
+        imgpath = settings.MEDIA_ROOT / img
+        img = Image.open(imgpath)
+        width, height = img.size
+        # print(f'Tamanho original: {width} X {height}')
+        newheight = round((newwidth * height) / width)
+        # print(f'Novo tamanho: {newwidth} X {newheight}')
+
+        if width > newwidth:
+            newimg = img.resize((newwidth, newheight), Image.ANTIALIAS)
+            newimg.save(imgpath, optimize=True, quality=75)
+            newimg.close()
